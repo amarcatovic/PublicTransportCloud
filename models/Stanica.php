36d9534c7e2c143546d34837f761c3232799cc11
Harun Smriko <62873114+harunsmrikoo@users.noreply.github.com>
2020-05-11 00:25:46 +0200

    Klase 14-26

---
+++ models/Stanica.php
@@ -0,0 +1,62 @@
+<?php
+class Stanica{
+
+    private $conn;
+    private $table = 'Stanica';
+
+    public $id_stanica;
+    public $naziv;
+    public $lat;
+    public $lng;
+    public $adresa;
+
+    // METODE
+    public function __construct($db) {
+      $this->conn = $db;
+    }
+
+    // GET
+    public function get() {
+      $query = 'CALL ';
+
+      $stmt = $this->conn->prepare($query);
+      $stmt->execute();
+
+      return $stmt;
+    }
+
+  public function read_single(){
+    $query = 'CALL ';
+
+      $stmt = $this->conn->prepare($query);
+      $stmt->bindParam(1, $this->id_korisnik);
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      $this->id_korisnik = $row['id_korisnik'];
+      $this->ime = $row['ime'];
+      $this->prezime = $row['prezime'];
+      $this->email = $row['email'];
+      $this->passwordHash = $row['passwordHash'];
+      $this->Grad = $row['Grad'];
+      $this->Uloga = $row['Uloga'];
+  }
+
+  // POST
+  public function create() {
+    $query = 'INSERT INTO ' . $this->table . ' (naziv) VALUES(:name)';
+
+  $stmt = $this->conn->prepare($query);
+  $this->naziv = htmlspecialchars(strip_tags($this->naziv));
+
+  $stmt-> bindParam(':name', $this->naziv);
+
+  if($stmt->execute()) {
+    return true;
+  }
+  printf("Greška: $s.\n", $stmt->error);
+
+  return false;
+  }
+}
